3bfcde13c6065967a224d4113c43175e3ea25878
Alex | Kronox <39801116+Kr0nox@users.noreply.github.com>
2022-12-27 15:37:52 +0100

    Added Java (#4)
    * Began implementing Java
    
    * postCopy is being ran now, fixed some java commands
    
    * Work on java
    
    * Add source path
    
    * Finished java
    
    * Moved Java Tasks into a singular file
    
    * Readded config
    
    Co-authored-by: Moritz R <c0derMo@users.noreply.github.com>

---
+++ .aoc/file_templates/java/Runner.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Error with the run command");
+            return;
+        }
+
+        // Load input file
+        File file = new File(args[1] + (args.length >= 3 && args[1].equals("test") ? "test" : "input"));
+        if (!file.exists()) {
+            System.out.println("File not found");
+            return;
+        }
+
+        // Parse input file
+        List<String> lines = new ArrayList<>();
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                lines.add(scanner.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        // Start task
+        Tasks t = new Tasks();
+        switch (args.length > 0 ? Integer.parseInt(args[0]) : 1) {
+            case 1 -> t.taskOne(lines.toArray(new String[0]));
+            case 2 -> t.taskTwo(lines.toArray(new String[0]));
+            default -> System.out.println("Task not found");
+        }
+    }
+}
