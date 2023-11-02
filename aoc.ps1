4a770a0564df479af34c83ed201cbdc090b34e41
Moritz R <c0derMo@users.noreply.github.com>
2023-11-02 16:21:30 +0100

    Initial recode commit

--- aoc.ps1
+++
@@ -1,9 +0,0 @@
-if (Test-Path -Path './.venv/' -PathType container) {
-    & ./.venv/Scripts/Activate.ps1
-} else {
-    echo "The usage of a virtual environment is recommended."
-    echo "This script searches for an enviroment in: '.venv'"
-}
-
-$allArgs = $PsBoundParameters.Values + $args
-python ./.aoc/aoc.py $allArgs
---
+++ go.mod
@@ -0,0 +1,3 @@
+module aoc-cli
+
+go 1.21.3
