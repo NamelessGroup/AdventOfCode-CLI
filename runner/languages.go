73e1761c4810698f11eb911465f843ac5ec8818f
Moritz R <c0derMo@users.noreply.github.com>
2023-11-22 18:26:13 +0100

    Removal of test language

--- runner/languages.go
+++ runner/languages.go
@@ -14,7 +14,6 @@ type Language interface {
 
 func ResolveLanguage(lang string) (Language, error) {
 	languageMap := map[string]Language{
-		"test":       languages.Test{},
 		"python":     languages.Python{},
 		"java":       languages.Java{},
 		"typescript": languages.TypeScript{},
